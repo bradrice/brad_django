2895c04115a29979190bda9345ffdc4f5578effb
Brad Rice <bradrice1@gmail.com>
2015-05-06 21:26:19 -0400

    static files setup with sass, too

--- web/settings.py
+++ web/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR), "static")
+
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
