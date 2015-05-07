3241cd36681fa3c3308f5218666e6b7f19d74b7a
Brad Rice <bradrice1@gmail.com>
2015-05-07 15:11:33 -0400

    Added header and first view

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from .views import HomeView
 
 urlpatterns = [
     # Examples:
-     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+     url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
