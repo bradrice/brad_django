7ade2eafc63207851c7c5d82d12eba6939783110
Brad Rice <bradrice1@gmail.com>
2015-05-12 16:57:15 -0400

    Added svg effects

---
+++ static/py/gen_svg.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+svg = """<svg version="1.1" id="b" class="box" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 10 10" enable-background="new 0 0 10 10" xml:space="preserve">
+	<rect fill="" width="10" height="10"/>
+	</svg>"""
+
+print(svg)
--- web/views.py
+++ web/views.py
@@ -1,10 +1,24 @@
 from django.views.generic import TemplateView
+import random
 
 class HomeView(TemplateView):
 
     template_name = 'index.html'
 
+    def build_dict(self, list):
+        ilist = [];
+        for i in range(1, 1000):
+            dict = {"item": i, "color": random.choice(list)}
+            ilist.append(dict)
+        return ilist
+
+
     def get_context_data(self, **kwargs):
+        purple_list = ["#A183B6", "#E8DFEF", "#C7B3D6", "#7E5999", "#5D337B"]
         context = super(HomeView, self).get_context_data(**kwargs)
         context['title'] = "Brad Rice home"
+
+        context['color_list'] =  ["#A183B6", "#E8DFEF", "#C7B3D6", "#7E5999", "#5D337B", "#DDF2AA", "#F7FCEB", "#ECF8CD", "#BBD776", "#8DAD41", "#FFF0B3", "#FFFCED", "#FFF7D3", "#E4D07E", "#B7A145"]
+        context['p_list'] = self.build_dict(purple_list)
         return context
+
