b427bee5b45c34fced9a8fc361bb5334dea5fee0
Brad Rice <bradrice1@gmail.com>
2015-05-15 16:18:36 -0400

    Added the ability to pass num argument to build_dict function

--- web/views.py
+++ web/views.py
@@ -5,9 +5,9 @@ class HomeView(TemplateView):
 
     template_name = 'index.html'
 
-    def build_dict(self, list):
+    def build_dict(self, list, num):
         ilist = [];
-        for i in range(1, 1000):
+        for i in range(1, num):
             dict = {"item": i, "color": random.choice(list)}
             ilist.append(dict)
         return ilist
@@ -18,7 +18,7 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['title'] = "Brad Rice home"
         color_list =  ["#A183B6", "#E8DFEF", "#C7B3D6", "#7E5999", "#5D337B", "#DDF2AA", "#F7FCEB", "#ECF8CD", "#BBD776", "#8DAD41", "#FFF0B3", "#FFFCED", "#FFF7D3", "#E4D07E", "#B7A145"]
-        context['p_list'] = self.build_dict(purple_list)
-        context['c_list'] = self.build_dict(color_list)
+        context['p_list'] = self.build_dict(purple_list, 800)
+        context['c_list'] = self.build_dict(color_list, 600)
         return context
 
