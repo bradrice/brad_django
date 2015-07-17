f69e3fafce7b1cf87a6c10920eb8fa948cc87f55
Brad Rice <bradrice1@gmail.com>
2015-07-17 08:39:53 -0400

    updated look of site, removed svgs

--- web/views.py
+++ web/views.py
@@ -11,23 +11,6 @@ class HomeView(TemplateView):
 
     template_name = 'index.html'
 
-    def build_dict(self, list, num):
-        ilist = [];
-        for i in range(1, num):
-            dict = {"item": i, "color": random.choice(list)}
-            ilist.append(dict)
-        return ilist
-
-
-    def get_context_data(self, **kwargs):
-        purple_list = ["#A183B6", "#E8DFEF", "#C7B3D6", "#7E5999", "#5D337B", "#ffffff"]
-        context = super(HomeView, self).get_context_data(**kwargs)
-        context['title'] = "Brad Rice home"
-        color_list =  ["#A183B6", "#E8DFEF", "#C7B3D6", "#7E5999", "#5D337B", "#DDF2AA", "#F7FCEB", "#ECF8CD", "#BBD776", "#8DAD41", "#FFF0B3", "#FFFCED", "#FFF7D3", "#E4D07E", "#B7A145", "#ffffff"]
-        context['p_list'] = self.build_dict(purple_list, 400)
-        context['c_list'] = self.build_dict(color_list, 280)
-        return context
-
 
 class ContactCreate(CreateView):
     model = Contact
